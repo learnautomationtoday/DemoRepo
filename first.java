6585afa2fb4fe1eb0978b0f0c4b1ffcf09b6af3b
learnautomationtoday <46396226+learnautomationtoday@users.noreply.github.com>
2019-08-13 19:33:00 +0530

    Update first.java

--- first.java
+++ first.java
@@ -3,7 +3,7 @@ public class first {
 
 	public static void main(String args[]) {
         // TODO Auto-generated method stub
-        System.out.Println("hello from java");
+        System.out.println("hello from java");
 
 	}
 
